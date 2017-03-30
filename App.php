61bfc2182056fd34ee3f3ac493ce8af2c3b809d2
tyme <s000193320@cs.mhu.edu>
2017-03-30 17:23:11 -0400

    Init Commit

---
+++ App.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+
+    <?php
+    $date = $_POST['date'];
+    $stime = $_POST['start'];
+    $etime = $_POST['end'];
+    $notes = $_POST['summary'];
+
+
+    ?>
+</head>
+<body>
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> to improve your experience.</p>
+<![endif]-->
+<!-- Add your site or application content here -->
+<form action="<?php echo basename($_SERVER['PHP_SELF']); ?>" method="post">
+    <fieldset>
+        <legend>Session:</legend>
+        Date(mm/dd/yyyy):<br> <input type="text" name="date"><br>
+        Start Time(:<br><input type="text" name="start"><br>
+        End Time:<br><input type="text" name="end"><br>
+        Session Summary:<br><textarea name="summary" rows="10" cols="30"></textarea>
+    </fieldset>
+    <input type="submit" value="Submit">
+    <?php
+    echo strtotime($date);
+    ?>
+
+</form>
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function (b, o, i, l, e, r) {
+        b.GoogleAnalyticsObject = l;
+        b[l] || (b[l] =
+            function () {
+                (b[l].q = b[l].q || []).push(arguments)
+            });
+        b[l].l = +new Date;
+        e = o.createElement(i);
+        r = o.getElementsByTagName(i)[0];
+        e.src = 'https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e, r)
+    }(window, document, 'script', 'ga'));
+    ga('create', 'UA-XXXXX-X', 'auto');
+    ga('send', 'pageview');
+</script>
+</body>
+</html>
