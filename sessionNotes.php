4c8b826d27b6d81f3be1b8768b02a587e9137146
tyme <s000193320@cs.mhu.edu>
2017-04-03 16:31:26 -0400

    Init Commit

--- sessionNotes.php
+++ sessionNotes.php
@@ -13,7 +13,12 @@
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 
     <?php
-    $Fname = $_POST['fname'];
+    $date = $_POST['date'];
+    $stime = $_POST['start'];
+    $etime = $_POST['end'];
+    $notes = $_POST['summary'];
+
+
     ?>
 </head>
 <body>
@@ -25,13 +30,15 @@
 <form action="<?php echo basename($_SERVER['PHP_SELF']); ?>" method="post">
     <fieldset>
         <legend>Session:</legend>
-            Date:<br> <input type="text" name="sdate"><br>
-            date:<br> <input type="date" name="test"><br>
-            Start Time:<br><input type="text" name="sstart"><br>
-            End Time:<br><input type="text" name="send"><br>
+            Date(mm/dd/yyyy):<br> <input type="text" name="date"><br>
+            Start Time(:<br><input type="text" name="start"><br>
+            End Time:<br><input type="text" name="end"><br>
             Session Summary:<br><textarea name="summary" rows="10" cols="30"></textarea>
     </fieldset>
     <input type="submit" value="Submit">
+    <?php
+        echo strtotime($date);
+    ?>
 
 </form>
 <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
